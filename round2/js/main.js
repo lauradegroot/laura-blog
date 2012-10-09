d4cd11b9ff809a8bc1722b07fdb099074276d850
Laura DeGroot <laurardegroot@gmail.com>
2012-10-09 15:29:23 -0400

    restructured to hide/show elements of the page, some jquery working, will perfect later

--- round2/js/main.js
+++ round2/js/main.js
@@ -1,7 +1,26 @@
 
 $(document).ready(function() {
- //  $('header li').hover(function(){
-	// $(this).addClass('animated bounce');
- //  });
-  
+	 $("header li#about").click(function() {
+	 	console.log("yep")
+	 	 $("#left .page").hide();
+		 $("#left #about").show();
+	 });
+
+	  $("header li#tutorials").click(function() {
+	 	console.log("yep")
+		 $("#left .page").hide();
+		 $("#left #tutorials").show();
+	 });
+
+	   $("header li#archives").click(function() {
+	 	console.log("yep")
+		 $("#left .page").hide();
+		 $("#left #archives").show();
+	 });
+
+	    $("header li#home").click(function() {
+	 	console.log("yep")
+		 $("#left .page").hide();
+		 $("#left #home").show();
+	 });
 });
